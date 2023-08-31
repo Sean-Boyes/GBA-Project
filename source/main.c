9194cdf074bd76483ce65ddd52eb21183f81bc24
Sean-Boyes <118757050+Sean-Boyes@users.noreply.github.com>
2023-08-30 20:00:09 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,10 +1,24 @@
-#include "../include/GBATypes.h"
+/*
 
-int value = 0;
+*(type*)address == casting address as a type and then derefrencing
+
+*/
+
+#include "../include/GBATypes.h"
+#include "../include/GBAVideo.h"
 
 int main()
 {
-    while(1);
+    int reg = DCNT_BG2 | DCNT_MODE3;
+    
+    *(unsigned int*)REG_BASE = reg;
 
+    //*(unsigned int*)0x04000000 = 0x0403;
+
+    ((unsigned short*)0x06000000)[120+80*240] = 0x001F;
+    ((unsigned short*)0x06000000)[136+80*240] = 0x03E0;
+    ((unsigned short*)0x06000000)[120+96*240] = 0x7C00;
+
+    while(1);
     return(0);
 }
