d453f83e6dda548bb34afbbb1f29e1c463eb9d15
Sean-Boyes <118757050+Sean-Boyes@users.noreply.github.com>
2023-08-30 19:59:13 -0700

    Created GBABase.h

---
+++ include/GBABase.h
@@ -0,0 +1,11 @@
+#ifndef GBA_BASE_H
+#define GBA_BASE_H
+
+#define EWRAM       0x02000000
+#define IWRAM       0x03000000
+#define REG_BASE    0x04000000
+#define MEM_PAL     0x05000000
+#define SRAM        0x0E000000
+#define VRAM        0x06000000
+
+#endif
