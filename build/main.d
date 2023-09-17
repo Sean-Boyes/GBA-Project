e56c6e2d75a8cee1a8e59a1de306d9eff3be4c6b
Sean-Boyes <118757050+Sean-Boyes@users.noreply.github.com>
2023-09-17 16:04:30 -0700

    Update main.d

--- build/main.d
+++ build/main.d
@@ -1,7 +1,13 @@
 main.o: C:/GBA-Project/source/main.c C:/GBA-Project/include/GBATypes.h \
- C:/GBA-Project/include/GBAVideo.h C:/GBA-Project/include/GBABase.h \
+ C:/GBA-Project/include/GBARegisters.h C:/GBA-Project/include/GBABase.h \
+ C:/GBA-Project/include/GBATypes.h C:/GBA-Project/include/GBAGraphics.h \
+ C:/GBA-Project/include/GBABase.h C:/GBA-Project/include/GBARegisters.h \
  C:/GBA-Project/include/GBATypes.h
 C:/GBA-Project/include/GBATypes.h:
-C:/GBA-Project/include/GBAVideo.h:
+C:/GBA-Project/include/GBARegisters.h:
 C:/GBA-Project/include/GBABase.h:
+C:/GBA-Project/include/GBATypes.h:
+C:/GBA-Project/include/GBAGraphics.h:
+C:/GBA-Project/include/GBABase.h:
+C:/GBA-Project/include/GBARegisters.h:
 C:/GBA-Project/include/GBATypes.h:
