da2761bcbc09f22568d51066ce693229c183d844
Sean-Boyes <118757050+Sean-Boyes@users.noreply.github.com>
2023-09-24 15:04:21 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -4,6 +4,8 @@
 
 */
 
+#include <math.h>
+
 #include "../include/GBATypes.h"
 #include "../include/GBARegisters.h"
 #include "../include/GBAGraphics.h"
@@ -25,30 +27,147 @@ int main()
 
     // mode3Line(1,1,10,10,0x7C00);
 
-    for(u16 i = 0; i < SCREENWIDTH; i++)
+    for(u16 i = 0; i <= SCREENWIDTH; ++i)
     {
-        for(u16 ii = 0; ii < SCREENHEIGHT; ii++)
+        for(u16 ii = 0; ii <= SCREENHEIGHT; ++ii)
         {
             mode3SetPixel(i,ii,0xFFFF);
         }
     }
 
     // from 1,1 to 3,3
-    mode3DrawLine(SCREENWIDTH/2,SCREENHEIGHT/2,SCREENWIDTH,SCREENHEIGHT,0x001F);
-    mode3DrawLine(SCREENWIDTH/2,SCREENHEIGHT/2,0,SCREENHEIGHT,0x03E0);
-    mode3DrawLine(SCREENWIDTH/2,SCREENHEIGHT/2,SCREENWIDTH,0,0x7C00);
-    mode3DrawLine(SCREENWIDTH/2,SCREENHEIGHT/2,0,0,0x0000);
+
+    // for (u16 x = 0; x < 24; x++)
+    // {
+    //     for (u16 y = 0; y < 16; y++)
+    //     {
+    //         mode3DrawLine(x*24,y*16,SCREENWIDTH-1,SCREENHEIGHT-1,0x001F);
+    //         mode3DrawLine(x*24,y*16,0,SCREENHEIGHT-1,0x03E0);
+    //         mode3DrawLine(x*24,y*16,SCREENWIDTH-1,0,0x7C00);
+    //         mode3DrawLine(x*24,y*16,0,0,0x7777);
+    //     }
+    // }
+    // mode3SetPixel(0,0,0);
+    // mode3SetPixel(239,159,0);
+    // mode3SetPixel(0,159,0);
+    // mode3SetPixel(239,0,0);
+
+    // Vertex test
+
+    u16 pointA[] = {200,140};
+    u16 pointB[] = {200,70};
+    u16 pointC[] = {50,70};
+    u16 pointD[] = {50,140};
+    u16 pointE[] = {200,140};
+    u16 pointF[] = {200,70};
+    u16 pointG[] = {50,70};
+    u16 pointH[] = {50,140};
+
+    float t = 0;
+    float r = 0;
+    float xScale = 50;
+    float yScale = 10;
+
+    // mode3DrawLine(1,1,240,2,0x0000);
+    // mode3DrawLine(240,2,1,1,0x0000);
+    // mode3DrawLine(1,1,3,180,0x0000);
+    // mode3DrawLine(3,180,1,1,0x0000);
 
     while(1)
-    {
-        // if(getVCount >= 160)
-        // {
-        //     setPage(0);
-        // }
-        // if(getVCount <= 80)
-        // {
-        //     setPage(1);
-        // }
+    {   
+
+
+        mode3DrawLine(pointA[0],pointA[1],pointB[0],pointB[1],0xFFFF);
+        mode3DrawLine(pointB[0],pointB[1],pointC[0],pointC[1],0xFFFF);
+        mode3DrawLine(pointC[0],pointC[1],pointD[0],pointD[1],0xFFFF);
+        mode3DrawLine(pointD[0],pointD[1],pointA[0],pointA[1],0xFFFF);
+        mode3DrawLine(pointE[0],pointE[1],pointF[0],pointF[1],0xFFFF);
+        mode3DrawLine(pointF[0],pointF[1],pointG[0],pointG[1],0xFFFF);
+        mode3DrawLine(pointG[0],pointG[1],pointH[0],pointH[1],0xFFFF);
+        mode3DrawLine(pointH[0],pointH[1],pointE[0],pointE[1],0xFFFF);
+        mode3DrawLine(pointE[0],pointE[1],pointA[0],pointA[1],0xFFFF);
+        mode3DrawLine(pointF[0],pointF[1],pointB[0],pointB[1],0xFFFF);
+        mode3DrawLine(pointG[0],pointG[1],pointC[0],pointC[1],0xFFFF);
+        mode3DrawLine(pointH[0],pointH[1],pointD[0],pointD[1],0xFFFF);
+
+        // left to right
+        pointA[0] = xScale*cos(t) + 120; 
+        pointB[0] = xScale*cos(t+1.57) + 120;
+        pointC[0] = xScale*cos(t+3.14) + 120; 
+        pointD[0] = xScale*cos(t+4.71) + 120; 
+
+        pointA[1] = yScale*sin(t) + 110; 
+        pointB[1] = yScale*sin(t+1.57) + 110;
+        pointC[1] = yScale*sin(t+3.14) + 110; 
+        pointD[1] = yScale*sin(t+4.71) + 110; 
+
+        pointE[0] = xScale*cos(t) + 120; 
+        pointF[0] = xScale*cos(t+1.57) + 120;
+        pointG[0] = xScale*cos(t+3.14) + 120; 
+        pointH[0] = xScale*cos(t+4.71) + 120; 
+
+        pointE[1] = yScale*sin(t) + 40; 
+        pointF[1] = yScale*sin(t+1.57) + 40;
+        pointG[1] = yScale*sin(t+3.14) + 40; 
+        pointH[1] = yScale*sin(t+4.71) + 40; 
+
+        // top to bottom 
+        if (read16Bit(REG_KEYINPUT, 0, 1) == 0)
+        {
+            pointA[1] = xScale*cos(r) + 80; 
+            pointB[1] = xScale*cos(r+1.57) + 80;
+            pointC[1] = xScale*cos(r+3.14) + 80; 
+            pointD[1] = xScale*cos(r+4.71) + 80; 
+
+            pointA[0] = yScale*sin(r) + 150; 
+            pointB[0] = yScale*sin(r+1.57) + 150;
+            pointC[0] = yScale*sin(r+3.14) + 150; 
+            pointD[0] = yScale*sin(r+4.71) + 150; 
+
+            pointE[1] = xScale*cos(r) + 80; 
+            pointF[1] = xScale*cos(r+1.57) + 80;
+            pointG[1] = xScale*cos(r+3.14) + 80; 
+            pointH[1] = xScale*cos(r+4.71) + 80; 
+
+            pointE[0] = yScale*sin(r) + 80; 
+            pointF[0] = yScale*sin(r+1.57) + 80;
+            pointG[0] = yScale*sin(r+3.14) + 80; 
+            pointH[0] = yScale*sin(r+4.71) + 80;
+        }
+
+
+        mode3DrawLine(pointA[0],pointA[1],pointB[0],pointB[1],0x001F);
+        mode3DrawLine(pointB[0],pointB[1],pointC[0],pointC[1],0x03E0);
+        mode3DrawLine(pointC[0],pointC[1],pointD[0],pointD[1],0x7C00);
+        mode3DrawLine(pointD[0],pointD[1],pointA[0],pointA[1],0x0000);
+        mode3DrawLine(pointE[0],pointE[1],pointF[0],pointF[1],0x001F);
+        mode3DrawLine(pointF[0],pointF[1],pointG[0],pointG[1],0x03E0);
+        mode3DrawLine(pointG[0],pointG[1],pointH[0],pointH[1],0x7C00);
+        mode3DrawLine(pointH[0],pointH[1],pointE[0],pointE[1],0x0000);
+        mode3DrawLine(pointE[0],pointE[1],pointA[0],pointA[1],0x001F);
+        mode3DrawLine(pointF[0],pointF[1],pointB[0],pointB[1],0x03E0);
+        mode3DrawLine(pointG[0],pointG[1],pointC[0],pointC[1],0x7C00);
+        mode3DrawLine(pointH[0],pointH[1],pointD[0],pointD[1],0x0000);
+
+
+        vu32 y = 0;
+        for (u16 x = 0; x < 0x0FFF; x++)
+        {
+            y = y + 3;
+            y = y + 1;
+            y = y + 1;
+        }
+
+        if (read16Bit(REG_KEYINPUT, 4,1) == 0) t = t + 0.1;
+        if (read16Bit(REG_KEYINPUT, 5,1) == 0) t = t - 0.1;
+        if (read16Bit(REG_KEYINPUT, 6,1) == 0) r = r + 0.1;
+        if (read16Bit(REG_KEYINPUT, 7,1) == 0) r = r - 0.1;
+        
+        
+        while(getVBlankStatus())
+        {
+
+        }; // VSync
     };
     return(0);
 }
