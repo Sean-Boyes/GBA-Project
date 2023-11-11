6f67e0de032285ff768237411a897087a0a9550f
Sean <118757050+Sean-Boyes@users.noreply.github.com>
2023-11-11 01:10:23 -0500

    Update main.d

--- build/main.d
+++ build/main.d
@@ -2,7 +2,9 @@ main.o: C:/GBA-Project/source/main.c C:/GBA-Project/include/GBATypes.h \
  C:/GBA-Project/include/GBARegisters.h C:/GBA-Project/include/GBABase.h \
  C:/GBA-Project/include/GBATypes.h C:/GBA-Project/include/GBAGraphics.h \
  C:/GBA-Project/include/GBABase.h C:/GBA-Project/include/GBARegisters.h \
- C:/GBA-Project/include/GBATypes.h
+ C:/GBA-Project/include/GBATypes.h C:/GBA-Project/include/GBAMath.h \
+ C:/GBA-Project/include/GBARegisters.h C:/GBA-Project/include/GBA3D.h \
+ C:/GBA-Project/include/GBAGraphics.h C:/GBA-Project/include/GBAMath.h
 C:/GBA-Project/include/GBATypes.h:
 C:/GBA-Project/include/GBARegisters.h:
 C:/GBA-Project/include/GBABase.h:
@@ -11,3 +13,8 @@ C:/GBA-Project/include/GBAGraphics.h:
 C:/GBA-Project/include/GBABase.h:
 C:/GBA-Project/include/GBARegisters.h:
 C:/GBA-Project/include/GBATypes.h:
+C:/GBA-Project/include/GBAMath.h:
+C:/GBA-Project/include/GBARegisters.h:
+C:/GBA-Project/include/GBA3D.h:
+C:/GBA-Project/include/GBAGraphics.h:
+C:/GBA-Project/include/GBAMath.h:
